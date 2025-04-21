440eaadf60b2601bc6145216ba79eef0ed2fa2e7
kang <swparabellum@gmail.com>
2025-04-21 15:53:11 +0900

    기능추가: 파일관리, oAuth 추가

---
+++ oauth/src/main/java/com/oneteam/file/FileControllerDocs.java
@@ -0,0 +1,7 @@
+package com.oneteam.file;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name="파일관리", description = "")
+public interface FileControllerDocs {
+}
---
+++ oauth/src/main/java/com/oneteam/oauth/OAuthClientControllerDocs.java
@@ -0,0 +1,7 @@
+package com.oneteam.oauth;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name="OAuth관리", description = "")
+public interface OAuthClientControllerDocs {
+}
