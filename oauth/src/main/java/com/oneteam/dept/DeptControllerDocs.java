2b0da36069e57dca8aab9af6b5aea6aef53448aa
swparabellum <swparabellum@gmail.com>
2025-04-21 23:47:33 +0900

    기능추가: 부서관리, 부서직원관리 기능 추가

---
+++ oauth/src/main/java/com/oneteam/dept/DeptControllerDocs.java
@@ -0,0 +1,7 @@
+package com.oneteam.dept;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name="부서관리", description = "")
+public interface DeptControllerDocs {
+}
---
+++ oauth/src/main/java/com/oneteam/dept/DeptUserControllerDocs.java
@@ -0,0 +1,7 @@
+package com.oneteam.dept;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name="부서직원관리", description = "")
+public interface DeptUserControllerDocs {
+}
